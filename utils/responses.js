dafe68c3d6ea2807d7424bb31acb95890aaf1126
Slava <>
2023-08-25 02:18:35 +0300

    Small fix

--- utils/responses.js
+++ utils/responses.js
@@ -1,7 +1,7 @@
 exports.created = (response, json) => response.status(201).json(json)
 exports.ok = (response, json) => response.status(200).json(json)
 exports.badRequest = (response, json) => response.status(400).json(json)
-exports.unauthorized = (response, json) => response.status(404).json(json)
+exports.unauthorized = (response, json) => response.status(401).json(json)
 exports.notFound = (response, json) => response.status(404).json(json)
 exports.error = (response, error) => {
     console.error(error);
